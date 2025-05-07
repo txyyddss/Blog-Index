ec18d7d15974605aa3132d80951e80ea19f6fa6e
txyyddss <tx@1919801.xyz>
2025-05-07 19:04:18 +0800

    Update Drawer.vue

--- src/components/basic/Drawer.vue
+++ src/components/basic/Drawer.vue
@@ -64,8 +64,8 @@ function onDrawerClick(e: Event) {
         </div>
         <div class="drawer__footer">
           <sketch-outlined class="footer-icon" />
-          <a href="https://github.com/EsunR/Blog-Index" target="_blank">
-            Driven by <b>EsunR/Blog-Index</b>
+          <a href="https://www.cloudflare-cn.com/developer-platform/products/pages/" target="_blank">
+            Driven by <b>Cloudflare Pages</b>
           </a>
         </div>
       </div>
